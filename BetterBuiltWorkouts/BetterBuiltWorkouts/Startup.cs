d85013979d96cc585e86d904a67f7d6c9b181dfb
Drew Crawford <thecodinghyrax@gmail.com>
2022-02-12 12:29:01 -0600

    Added scaffolding to allow register/login styling. Added fname and lname to user.

--- BetterBuiltWorkouts/BetterBuiltWorkouts/Startup.cs
+++ BetterBuiltWorkouts/BetterBuiltWorkouts/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BetterBuiltWorkouts.Models;
 
 namespace BetterBuiltWorkouts
 {
@@ -27,13 +28,15 @@ namespace BetterBuiltWorkouts
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+     
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
         }
 
