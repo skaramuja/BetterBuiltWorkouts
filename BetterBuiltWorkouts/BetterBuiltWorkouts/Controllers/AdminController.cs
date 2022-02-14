e78a02d992e40cc731e0d94914c1551b99ea54fa
Drew Crawford <thecodinghyrax@gmail.com>
2022-02-13 18:34:48 -0600

    Admin added plus workout controller

---
+++ BetterBuiltWorkouts/BetterBuiltWorkouts/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuiltWorkouts.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Welcome to the Admin page!";
+            return View();
+        }
+    }
+}
