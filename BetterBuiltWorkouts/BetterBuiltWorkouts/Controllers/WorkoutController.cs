53f63b077eb3be0a6182dfab7173fcb333a446af
thecodinghyrax <thecodinghyrax@gmail.com>
2022-04-08 15:00:34 -0500

    Still working on Plan edit view

--- BetterBuiltWorkouts/BetterBuiltWorkouts/Controllers/WorkoutController.cs
+++ BetterBuiltWorkouts/BetterBuiltWorkouts/Controllers/WorkoutController.cs
@@ -14,14 +14,7 @@ namespace BetterBuiltWorkouts.Controllers
             data = new WorkoutUnitOfWork(ctx);
         }
 
-        public IActionResult Details(int id)
-        {
-            var exercise = data.GetExercise(id);
-            ViewBag.ExerciseType = data.GetExerciseType(exercise.ExerciseTypeID);
-            ViewBag.Types = data.ListAllExerciseTypes().ToList();
-            return View(exercise);
-        }
-
+        // Plan Section
         [Route("CreatePlan-Workout")]
         public IActionResult CreatePlan()
         {
@@ -33,6 +26,79 @@ namespace BetterBuiltWorkouts.Controllers
         {
             var model = data.GetPlan(id);
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult PlanDelete(Plan plan)
+        {
+            data.DeletePlan(plan);
+            data.Save();
+            return RedirectToAction("CreatePlan", "Workout");
+        }
+
+        [HttpGet]
+        public IActionResult PlanEdit(int id)
+        {
+            ViewBag.Verb = "Edit";
+            ViewBag.Action = "Save";
+            ViewBag.Exercises = data.ListOfExercises("all");
+            Plan plan = data.GetPlan(id);
+            return View(plan);
+        }
+
+        [HttpPost]
+        public IActionResult PlanEdit(Plan plan)
+        {
+            if (ModelState.IsValid)
+            {
+                if (plan.PlanId == 0)
+                {
+                    //plan.CreatedBy = User.Identity.Name;
+                    data.Plans.Insert(plan);
+                }
+                else
+                {
+                    data.Plans.Update(plan);
+                }
+                data.Save();
+                return RedirectToAction("CreatePlan", "Workout");
+            }
+            else
+            {
+                ViewBag.Verb = "Edit";
+                ViewBag.Action = "Save";
+                ViewBag.Exercises = data.ListOfExercises("all");
+                return View(plan);
+
+            }
+        }
+
+
+        // Exercise section
+        [Route("Exercises-Workout")]
+        public IActionResult Exercises(ExerciseListViewModel model)
+        {
+            var exerciseTypes = data.ListOfExercises(model.ActiveExerciseType);
+            model.ExerciseTypes = data.ListAllExerciseTypes().ToList();
+            model.Exercises = exerciseTypes.ToList();
+
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var exercise = data.GetExercise(id);
+            ViewBag.ExerciseType = data.GetExerciseType(exercise.ExerciseTypeID);
+            ViewBag.Types = data.ListAllExerciseTypes().ToList();
+            return View(exercise);
+        }
+
+        [HttpGet]
+        public ViewResult CreateExercise()
+        {
+            ViewBag.Action = "Create";
+            ViewBag.Types = data.ListAllExerciseTypes().ToList();
+            return View("Edit", new Exercise());
         }
 
         [HttpPost]
@@ -81,32 +147,11 @@ namespace BetterBuiltWorkouts.Controllers
         }
 
 
-        [HttpGet]
-        public ViewResult CreateExercise()
-        {
-            ViewBag.Action = "Create";
-            ViewBag.Types = data.ListAllExerciseTypes().ToList();
-            return View("Edit", new Exercise());
-        }
-
-
-
+        // Preform Section
         [Route("Perform-Workout")]
         public IActionResult Perform()
         {
             return View();
-        }
-
-
-
-        [Route("Exercises-Workout")]
-        public IActionResult Exercises(ExerciseListViewModel model)
-        {
-            var exerciseTypes = data.ListOfExercises(model.ActiveExerciseType);
-            model.ExerciseTypes = data.ListAllExerciseTypes().ToList();
-            model.Exercises = exerciseTypes.ToList();
-
-            return View(model);
         }
 
     }
---
+++ BetterBuiltWorkouts/BetterBuiltWorkouts/ViewModels/PlanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BetterBuiltWorkouts.Models
+{
+    public class PlanViewModel : Plan
+    {
+        public Plan Plan { get; set; }
+        public List<Exercise> Exercises { get; set; }
+    }
+}
