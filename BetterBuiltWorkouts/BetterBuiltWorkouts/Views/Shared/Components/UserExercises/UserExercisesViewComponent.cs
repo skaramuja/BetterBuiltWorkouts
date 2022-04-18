1b796f8f16c7559ecd5996ec6d7084dc133ed245
thecodinghyrax <thecodinghyrax@gmail.com>
2022-04-18 14:28:42 -0500

    Partial user exercises is functional

---
+++ BetterBuiltWorkouts/BetterBuiltWorkouts/Views/Shared/Components/UserExercises/UserExercisesViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using BetterBuiltWorkouts.Data;
+using BetterBuiltWorkouts.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuiltWorkouts.Views.Workout.Components
+{
+    public class UserExercisesViewComponent : ViewComponent
+    {
+        private IRepository<Exercise> data { get; set; }
+        public UserExercisesViewComponent(IRepository<Exercise> repo) => data = repo;
+
+        public IViewComponentResult Invoke(string userName, string exerciseTypeID)
+        {
+            QueryOptions<Exercise> options;
+            if (exerciseTypeID == "all")
+            {
+                options = new QueryOptions<Exercise> { Where = e => e.CreatedBy == userName };
+            }
+            else
+            {
+                options = new QueryOptions<Exercise> { Where = e => e.CreatedBy == userName && e.ExerciseTypeID == exerciseTypeID };
+            }
+            var userExercises = data.List(options);
+            var model = new ExerciseListViewModel() { Exercises = userExercises };
+            return View("Default.cshtml", model);
+        }
+    }
+}
